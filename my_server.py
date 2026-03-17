65bcce84a41d0c776c0ea6909ab4669aad699ab6
Albert Sunil David <albert.sunild@hotmail.com>
2026-03-17 20:41:48 +0530

    Update my_server.py

--- my_server.py
+++ my_server.py
@@ -291,7 +291,7 @@ def get_healthnav_links():
     return RESPONSE_PAYLOAD
 
 if __name__ == "__main__":
-    mcp.run()
+    # mcp.run()
     # mcp.run(transport="streamable-http", host="127.0.0.1", port=8080)
 
 
